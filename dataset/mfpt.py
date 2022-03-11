5907ab0589747f09d91efadc4793744048568c89
diegoaichele <37969116+diegoaichele@users.noreply.github.com>
2022-03-11 15:32:52 -0300

    add mfpt, pu and xjtu _ver0

---
+++ dataset/mfpt.py
@@ -0,0 +1,14 @@
+
+import torch
+import torch.utils.data as data
+
+
+class MFPT(data.Dataset):
+    #Aquí se dejan variables
+    # de archivos y carpetas  
+    def __init__(
+        self,
+        root: str,
+    )->None:
+        self.root = root
+        
