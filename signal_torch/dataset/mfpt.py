033fbf1745769dbbe85370066e9aa73d4b5de163
diegoaichele <37969116+diegoaichele@users.noreply.github.com>
2022-09-21 23:46:45 -0300

    feat: MPFT Final Version

--- signal_torch/dataset/mfpt.py
+++ signal_torch/dataset/mfpt.py
@@ -1,11 +1,10 @@
-
+import glob
 import os
 import torch
 import scipy.io
 import torch.utils.data as data
 from typing import Optional, Callable
 from torchvision.datasets.utils import download_url, _extract_zip
-
 class MFPT(data.Dataset):
     #Aquí se dejan variables
     # de archivos y carpetas  
@@ -15,12 +14,16 @@ class MFPT(data.Dataset):
         self,
         root: str,
         new_length: int = 312,
+        overlap: float = 0.0,
         transform: Optional[Callable] = None,
         download: bool = True,
     )->None:
         self.root = root
         self.new_length = new_length
         self.transform = transform
+        assert overlap < 1  and overlap >=0, "Values of overlap is [0,1)"
+        self.overlap = int((1-overlap)*new_length)
+        assert self.overlap != 0, "Value of overlap is too big"
         if download:
             self.download()
         
@@ -35,41 +38,22 @@ class MFPT(data.Dataset):
         
         
     def _load_data(self):    
-        files_mat =[os.path.join(self.root,"MFPT Fault Data Sets","1 - Three Baseline Conditions","baseline_1.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","1 - Three Baseline Conditions","baseline_2.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","1 - Three Baseline Conditions","baseline_3.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","2 - Three Outer Race Fault Conditions","OuterRaceFault_1.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","2 - Three Outer Race Fault Conditions","OuterRaceFault_2.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","2 - Three Outer Race Fault Conditions","OuterRaceFault_3.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_1.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_2.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_3.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_4.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_5.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_6.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","3 - Seven More Outer Race Fault Conditions","OuterRaceFault_vload_7.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_1.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_2.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_3.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_4.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_5.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_6.mat"),
-                    os.path.join(self.root,"MFPT Fault Data Sets","4 - Seven Inner Race Fault Conditions","InnerRaceFault_vload_7.mat")]
+        files_mat = glob.glob(os.path.join(self.root, "**", "*.mat"),recursive=True)[:-4]   
         # 0: BaseLine, 1:Outer, 2:Inner
-        damage_dict = {0:0,1:0,2:0,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:2,14:2,15:2,16:2,17:2,18:2,19:2}
-        
+        damage_list = [0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2]
         data_total = ()
         target_total = ()
         
         for index, file_mat in enumerate(files_mat):
-            tmp_data = scipy.io.loadmat( file_mat )['bearing']
             try:
-                data = torch.from_numpy(tmp_data['gs'][0][0][:-(len(tmp_data["gs"][0][0])%self.new_length)]).reshape(-1,self.new_length).unbind()
-            except:
-                data = torch.from_numpy(tmp_data["gs"][0][0][:-(len(tmp_data["gs"][0][0])%self.new_length)]).reshape(-1,self.new_length).unbind()
-            target = torch.Tensor( (damage_dict[index], int(tmp_data['sr'][0][0][0][0]), int(tmp_data['rate'][0][0][0][0]), int(tmp_data['load'][0][0][0][0]) ) )
-            data_total = data_total + data
-            target_total = target_total + target.repeat(len(data),1).unbind() 
+                tmp_data = scipy.io.loadmat( file_mat )['bearing']
+                tmp_data = torch.from_numpy(tmp_data['gs'][0][0]).ravel()
+                data = tmp_data.unfold(0, self.new_length, self.overlap).unbind()
+                target = torch.Tensor([damage_list[index]])  
+                data_total = data_total + data
+                target_total = target_total + target.repeat(len(data),1).unbind() 
+            except: 
+                print("Error Loading: ",file_mat)
         
         return data_total, target_total
     
@@ -86,4 +70,4 @@ class MFPT(data.Dataset):
         return len(self.data)
     
     def __repr__(self) -> str:
-        return "MFPT Dataset  (" + str(len(self.data)) + " samples)" + "0: BaseLine, 1:Outer, 2:Inner"
+        return "MFPT Dataset  (" + str(len(self.data)) + " samples) " + "0: BaseLine, 1:Outer, 2:Inner"
